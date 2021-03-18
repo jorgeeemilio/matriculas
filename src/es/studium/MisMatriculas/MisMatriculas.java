142480c41d29e1b6fbafedb926f7c931154cf3b5
Jorge Rodríguez <jorge@grupostudium.com>
2021-03-18 08:24:53 +0100

    Inicio

---
+++ src/es/studium/MisMatriculas/MisMatriculas.java
@@ -0,0 +1,14 @@
+package es.studium.MisMatriculas;
+
+public class MisMatriculas
+{
+	public static void main(String[] args)
+	{
+		// Leer fichero
+		// Procesar información
+		// 1º Averiguar si matrícula NUEVA o ANTIGUA
+		// 2º Correcta o No
+		// Generar otro fichero llamado correctas.txt
+		// Solo con las matriculas correctas
+	}
+}
